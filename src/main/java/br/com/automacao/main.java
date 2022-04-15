d3a34daf174085c46caeb8f7613df9308a9febb9
renato silva <renatosilva10000000@gmail.com>
2022-04-15 16:30:41 -0300

    tratamento de acentuação no texto

--- src/main/java/br/com/automacao/main.java
+++ src/main/java/br/com/automacao/main.java
@@ -5,18 +5,14 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.util.Scanner;
 import java.util.stream.Collectors;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
+import br.com.automacao.utils.Utils;
 
 public class main {
 
+	static Utils utils = new Utils();
+	
 	public static void main(String[] args) {
 
 		
@@ -24,58 +20,27 @@ public class main {
 		
 		String pessoa = "Neymar";
 		
-
-		
-		
 					
 				try {
 					
-				//	String encoding = "UTF-8";
+					String urlWikipediaSearch = "https://pt.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=" + URLEncoder.encode(pessoa, "UTF-8");
 					
+					System.out.println("\n" + urlWikipediaSearch);
 					
-					//Search the google for Wikipedia Links
-					//Document google = Jsoup.connect("https://www.google.com/search?q=" + URLEncoder.encode("Neymar", encoding)).userAgent("Mozilla/5.0").get();
-					
-
-					//Get the first link about Wikipedia
-					//String wikipediaURL = google.getElementsByTag("cite").get(0).text();
-					
-					//Use Wikipedia API to get JSON File
-					String wikipediaApiJSON = "https://pt.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles="
-							+ URLEncoder.encode(pessoa, "UTF-8");
-					
-
-					
-					//ByteBuffer byteBuffer = StandardCharsets.UTF_8.encode(wikipediaApiJSON);
-									
-					
-					//Let's see what it found
-					System.out.println(wikipediaApiJSON);
-					
-					//"extract":" the summary of the article
-					HttpURLConnection httpcon = (HttpURLConnection) new URL(wikipediaApiJSON).openConnection();
-					httpcon.addRequestProperty("User-Agent", "Mozilla/5.0");
-
-				
+					HttpURLConnection httpcon = (HttpURLConnection) new URL(urlWikipediaSearch).openConnection();
 					BufferedReader in = new BufferedReader(new InputStreamReader(httpcon.getInputStream(), "UTF-8"));
-					
-					//Read line by line
 					String responseSB = in.lines().collect(Collectors.joining());
-					
-	
-					
-					
 					in.close();
 					
-					//Print the result for us to see
-					//System.out.println(responseSB);
-					String result = responseSB.split("extract\":\"")[1];
-					//System.out.println(result);
+					String resultadoDaBusca = responseSB.split("extract\":\"")[1];
+
+					resultadoDaBusca = utils.ajusteAcentuacao(resultadoDaBusca);
+					resultadoDaBusca = utils.limparCaracteresEspeciais(resultadoDaBusca);
 					
-					//Tell only the 150 first characters of the result
-					String textToTell = result.length() > 250 ? result.substring(0, 250) : result;
-					System.out.println(textToTell + "...");
+
+					System.out.println("\n" + resultadoDaBusca);
 					
+				
 					
 				} catch (Exception ex) {
 					ex.printStackTrace();
